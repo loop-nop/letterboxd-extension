cb55b296daa60cbcd9cdd68bb489ca7591299c3f
nn <86272232+loop-nop@users.noreply.github.com>
2023-05-15 19:42:10 +0000

    cleanup

--- background.js
+++
@@ -1,9 +0,0 @@
-// browser.tabs.onUpdated.addListener((id, changeInfo, tab) => {
-//     initializePageAction(tab);
-//   });
-
-
-
-// function initializePageAction(tab) {
-//     browser.pageAction.show(tab.id)
-// }
--- main.js
+++ main.js
@@ -1,137 +1,136 @@
 /* 
 This code will run ONLY on letterboxd/(username)/list/*
 */
+{
+    const __MAGIC_WORD = "random"
 
-const __MAGIC_WORD = "random"
-
-
-
-init()
+    init()
 
 
-function init(){
-    create_random_button()
+    function init(){
+        create_random_button()
 
-    if(hasMagicNumber()){
-        // let popup = showMoviePopup()
-        // popup.appendChild( getRandomMovie() )
-        goToRandomMovie()
+        if(hasMagicNumber()){
+            // let popup = showMoviePopup()
+            // popup.appendChild( getRandomMovie() )
+            goToRandomMovie()
+        }
     }
-}
 
-function hasMagicNumber(){
-    if (window.location.search.match(__MAGIC_WORD)){
-        return true
-    }else{
-        return false
-    }
-}
-
-function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
-  }
-
-function getRandomMovieElement(){
-    console.log("random now")
-
-    let movies = document.getElementById("content").getElementsByClassName("poster-list")[0].getElementsByTagName("li")
-    let randomMovieNumber = getRandomIntInclusive(0, movies.length - 1)
-    return movies[randomMovieNumber] 
-}
-
-function goToRandomMovie(){
-    console.log("goToRandomMovie");
-    let newURL
-    newURL = getRandomMovieElement().getElementsByClassName("film-poster")[0].getAttribute("data-target-link")
-    if (newURL == null) {
-        newURL = getRandomMovieElement().getElementsByTagName("a")[0].href;
-        document.location.href = newURL
-    }else{
-        document.search = ""
-        document.location.href = "https://letterboxd.com" + newURL
-    }
-}
-
-
-function create_random_button(){
-    let randomButton =  document.createElement("button")
-    randomButton.classList = "button clipboardtrigger has-icon"
-    randomButton.innerHTML = __MAGIC_WORD 
-
-    let ButtonSpace = document.createElement("li")
-    ButtonSpace.classList.add("panel-signin")
-    ButtonSpace.appendChild(randomButton);
-    ButtonSpace.onclick = goToRandomPage;
-
-    let UserPanel = document.getElementById("userpanel").getElementsByTagName("ul")[0]
-    let InsertTarget = UserPanel.getElementsByTagName("li")[0]
-    let RandomButton =  UserPanel.insertBefore(ButtonSpace, InsertTarget)
-}
-
-function goToRandomPage(){
-    console.log("goToRandomPage");
-    let pages = document.getElementsByClassName("paginate-pages")[0]
-    if (!pages) {
-        goToRandomMovie();
-        return;
-    }
-    pages = pages.getElementsByTagName("li")
-
-    let maxPage = pages[pages.length - 1].outerText
-    let randomPage = getRandomIntInclusive(1, maxPage)
-    let curentPage = getPageNumber(document.URL) 
-    let newUrl = getBaseURL(document.URL)
-
-    console.log(randomPage);
-    console.log(curentPage);
-    if (randomPage == curentPage) {
-        goToRandomMovie()
-    }else{
-        if (randomPage > 1){
-            newUrl += "page/" + randomPage + "?" + __MAGIC_WORD
-            document.location.href = newUrl
+    function hasMagicNumber(){
+        if (window.location.search.match(__MAGIC_WORD)){
+            return true
         }else{
-            newUrl = newUrl.split("page/")[0] + "?" + __MAGIC_WORD
-            document.location.href = newUrl
+            return false
         }
     }
-}
 
-function getPageNumber(URL){
-    if (URL.search("/?") != -1){
-        URL = URL.split("?")[0]
+    function getRandomIntInclusive(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
     }
 
-    let pageNumber = 1
-    if(document.URL.match("/page/")){
-        pageNumber = document.URL.split("/page/")[1]
-        if ( pageNumber.split().length > 1 ){
-            pageNumber = pageNumber.split("/")[0]
+    function getRandomMovieElement(){
+        console.log("random now")
+
+        let movies = document.getElementById("content").getElementsByClassName("poster-list")[0].getElementsByTagName("li")
+        let randomMovieNumber = getRandomIntInclusive(0, movies.length - 1)
+        return movies[randomMovieNumber] 
+    }
+
+    function goToRandomMovie(){
+        console.log("goToRandomMovie");
+        let newURL
+        newURL = getRandomMovieElement().getElementsByClassName("film-poster")[0].getAttribute("data-target-link")
+        if (newURL == null) {
+            newURL = getRandomMovieElement().getElementsByTagName("a")[0].href;
+            document.location.href = newURL
+        }else{
+            document.search = ""
+            document.location.href = "https://letterboxd.com" + newURL
         }
     }
-    return pageNumber
-}
 
-function getBaseURL(URL){
-    URL = URL.toLowerCase()
-    if (URL.search("/?") != -1){
-        URL = URL.split("?")[0]
+
+    function create_random_button(){
+        let randomButton =  document.createElement("button")
+        randomButton.classList = "button clipboardtrigger has-icon"
+        randomButton.innerHTML = "random" 
+
+        let ButtonSpace = document.createElement("li")
+        ButtonSpace.classList.add("panel-signin")
+        ButtonSpace.appendChild(randomButton);
+        ButtonSpace.onclick = goToRandomPage;
+
+        let UserPanel = document.getElementById("userpanel").getElementsByTagName("ul")[0]
+        let InsertTarget = UserPanel.getElementsByTagName("li")[0]
+        let RandomButton =  UserPanel.insertBefore(ButtonSpace, InsertTarget)
     }
-    if (URL.search("page/")){
-        URL = URL.split("page/")[0]
+
+    function goToRandomPage(){
+        console.log("goToRandomPage");
+        let pages = document.getElementsByClassName("paginate-pages")[0]
+        if (!pages) {
+            goToRandomMovie();
+            return;
+        }
+        pages = pages.getElementsByTagName("li")
+
+        let maxPage = pages[pages.length - 1].outerText
+        let randomPage = getRandomIntInclusive(1, maxPage)
+        let curentPage = getPageNumber(document.URL) 
+        let newUrl = getBaseURL(document.URL)
+
+        console.log(randomPage);
+        console.log(curentPage);
+        if (randomPage == curentPage) {
+            goToRandomMovie()
+        }else{
+            if (randomPage > 1){
+                newUrl += "page/" + randomPage + "?" + __MAGIC_WORD
+                document.location.href = newUrl
+            }else{
+                newUrl = newUrl.split("page/")[0] + "?" + __MAGIC_WORD
+                document.location.href = newUrl
+            }
+        }
     }
 
-    return URL
-}
+    function getPageNumber(URL){
+        if (URL.search("/?") != -1){
+            URL = URL.split("?")[0]
+        }
 
-function showMoviePopup(){
-    let popup = document.createElement("ul")
-    popup.classList.add("smenu-menu")
-    popup.id = "random-popup"
-    popup.style = "position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); display: block;"
+        let pageNumber = 1
+        if(document.URL.match("/page/")){
+            pageNumber = document.URL.split("/page/")[1]
+            if ( pageNumber.split().length > 1 ){
+                pageNumber = pageNumber.split("/")[0]
+            }
+        }
+        return pageNumber
+    }
 
-    document.getElementById("content").appendChild(popup)
-    return popup
+    function getBaseURL(URL){
+        URL = URL.toLowerCase()
+        if (URL.search("/?") != -1){
+            URL = URL.split("?")[0]
+        }
+        if (URL.search("page/")){
+            URL = URL.split("page/")[0]
+        }
+
+        return URL
+    }
+
+    function showMoviePopup(){
+        let popup = document.createElement("ul")
+        popup.classList.add("smenu-menu")
+        popup.id = "random-popup"
+        popup.style = "position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); display: block;"
+
+        document.getElementById("content").appendChild(popup)
+        return popup
+    }
 }
