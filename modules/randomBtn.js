7398c3b3a24c9d99d2a1224f7f0d7ca463c814b3
qtc <86272232+loop-nop@users.noreply.github.com>
2023-11-05 00:46:24 +0300

    ui draft

--- modules/randomBtn.js
+++ modules/randomBtn.js
@@ -9,162 +9,165 @@ const fastRandom = false;
 
 init()
 
+// test
+maximazeContent()
+prepareGrid(0, 4)
 
 function init(){
-    const magicIndex = getMagicWord()
-    //console.debug(magicIndex)
-    if(magicIndex != null){
-        // let popup = showMoviePopup()
-        // popup.appendChild( getRandomMovie() )
+	const magicIndex = getMagicWord()
+	//console.debug(magicIndex)
+	if(magicIndex != null){
+			// let popup = showMoviePopup()
+			// popup.appendChild( getRandomMovie() )
 
-        openMovie(magicIndex)
-        return
-    }else{
-        createRandomButton()
-    }
+			openMovie(magicIndex)
+			return
+	}else{
+			createRandomButton()
+	}
 }
 
 function start(){
-    button.disabled = true;
-    const totalPages = getPagesCount(document);
-    if (totalPages > 1) {
-        if (fastRandom){
-            const randomPage = getRandomIntInclusive(1, totalPages);
-            goToMovie(randomPage)
-        }else{
-            const firstPageUrl = getPageUrl(1);
-            const paginationSize = getPageSize(firstPageUrl);
-            const lastPageUrl = getPageUrl(totalPages);
-            const lastPageSize = getPageSize(lastPageUrl);
+	button.disabled = true;
+	const totalPages = getPagesCount(document);
+	if (totalPages > 1) {
+			if (fastRandom){
+					const randomPage = getRandomIntInclusive(1, totalPages);
+					goToMovie(randomPage)
+			}else{
+					const firstPageUrl = getPageUrl(1);
+					const paginationSize = getPageSize(firstPageUrl);
+					const lastPageUrl = getPageUrl(totalPages);
+					const lastPageSize = getPageSize(lastPageUrl);
 
-            Promise.all([paginationSize, lastPageSize]).then( (values) => {
-                const totalMoviesCount = values[0] * (totalPages - 1) + values[1];
-                const randomMovieIndex = getRandomIntInclusive(1, totalMoviesCount);
-                const moviePage = Math.floor(randomMovieIndex/values[0]);
-                const movieIndex = randomMovieIndex%values[0];
+					Promise.all([paginationSize, lastPageSize]).then( (values) => {
+							const totalMoviesCount = values[0] * (totalPages - 1) + values[1];
+							const randomMovieIndex = getRandomIntInclusive(1, totalMoviesCount);
+							const moviePage = Math.floor(randomMovieIndex/values[0]);
+							const movieIndex = randomMovieIndex%values[0];
 
-                console.debug("pagination =", values[0], "lastPageSize =", values[1], "total:", totalMoviesCount, totalPages, randomMovieIndex, moviePage, movieIndex)
-                goToMovie(moviePage, movieIndex);
-            });
-        }
-    }else{
-        goToMovie(1)
-    }
+							console.debug("pagination =", values[0], "lastPageSize =", values[1], "total:", totalMoviesCount, totalPages, randomMovieIndex, moviePage, movieIndex)
+							goToMovie(moviePage, movieIndex);
+					});
+			}
+	}else{
+			goToMovie(1)
+	}
 }
 
 function goToMovie(pageNumber, movieIndex = -1){
-    const currentPage = getPageNumber(document.URL);
-    console.debug("goToMovie", pageNumber, movieIndex)
+	const currentPage = getPageNumber(document.URL);
+	console.debug("goToMovie", pageNumber, movieIndex)
 
-    if (currentPage == pageNumber) {
-        openMovie(movieIndex);
-    }else{
-        const movieUrl = getPageUrl(pageNumber);
-        if (movieUrl){
-            const url  = [movieUrl, "?", MAGIC_WORD, "=", movieIndex].join("");
-            document.location.href = url;
-        }else{
-            document.search = "";
-            document.location.href = "https://letterboxd.com";
-        }
-    }
+	if (currentPage == pageNumber) {
+			openMovie(movieIndex);
+	}else{
+			const movieUrl = getPageUrl(pageNumber);
+			if (movieUrl){
+					const url  = [movieUrl, "?", MAGIC_WORD, "=", movieIndex].join("");
+					document.location.href = url;
+			}else{
+					document.search = "";
+					document.location.href = "https://letterboxd.com";
+			}
+	}
 }
 
 function openMovie(index){
-    const movieCard = getMovieElement(index);
-    let url = movieCard.getElementsByClassName("film-poster")[0].getAttribute("data-target-link");
-    if (url != null){
-        url = "https://letterboxd.com" + url;
-    }
-    else{
-        url = movieCard.getElementsByTagName("a")[0].href;
-    }
+	const movieCard = getMovieElement(index);
+	let url = movieCard.getElementsByClassName("film-poster")[0].getAttribute("data-target-link");
+	if (url != null){
+			url = "https://letterboxd.com" + url;
+	}
+	else{
+			url = movieCard.getElementsByTagName("a")[0].href;
+	}
 
-    //console.debug(movieCard, "https://letterboxd.com" + url);
-    if (url){
-        document.search = "";
-        document.location.href = url;
-    }else{
-        console.error("failed to get movie url");
-    }
+	//console.debug(movieCard, "https://letterboxd.com" + url);
+	if (url){
+			document.search = "";
+			document.location.href = url;
+	}else{
+			console.error("failed to get movie url");
+	}
 }
 
 function getMagicWord(){
-    const params = (new URL(document.URL)).searchParams;
-    const magicValue = params.get(MAGIC_WORD);
-    return magicValue;
+	const params = (new URL(document.URL)).searchParams;
+	const magicValue = params.get(MAGIC_WORD);
+	return magicValue;
 }
 
 function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min);
-    // The maximum is inclusive and the minimum is inclusive
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1) + min);
+	// The maximum is inclusive and the minimum is inclusive
 }
 
 async function getPageSize(url){
-    const listPageText = await fetch(url).then((r) => {
-        const res = r.text();
-        return res;
-    });
-    const templ = document.createElement("template");
-    templ.innerHTML = listPageText;
-    const movies = getPageMovies(templ.content)
-    //console.debug("pagination =", movies.length)
-    return movies.length;
+	const listPageText = await fetch(url).then((r) => {
+			const res = r.text();
+			return res;
+	});
+	const templ = document.createElement("template");
+	templ.innerHTML = listPageText;
+	const movies = getPageMovies(templ.content)
+	//console.debug("pagination =", movies.length)
+	return movies.length;
 }
 
 function getPageMovies(pageContent){
-    const movies = pageContent.getElementById("content").getElementsByClassName("poster-list")[0].getElementsByTagName("li")
-    return movies;
+	const movies = pageContent.getElementById("content").getElementsByClassName("poster-list")[0].getElementsByTagName("li")
+	return movies;
 }
 
 function getMovieElement(index){
-    const movies = getPageMovies(document);
-    if (index < 1){
-        index = getRandomIntInclusive(1, movies.length)
-    }
-    
-    //console.debug(index,  movies[index-1]);
-    if (movies.length < index){
-        console.error("wrong movie index")
-        return
-    }
-    
-    return movies[index-1];
+	const movies = getPageMovies(document);
+	if (index < 1){
+			index = getRandomIntInclusive(1, movies.length)
+	}
+	
+	//console.debug(index,  movies[index-1]);
+	if (movies.length < index){
+			console.error("wrong movie index")
+			return
+	}
+	
+	return movies[index-1];
 }
 
 function createRandomButton(){
-    const randomButton =  document.createElement("button")
-    randomButton.classList = "button clipboardtrigger has-icon"
-    randomButton.innerHTML = MAGIC_WORD
+	const randomButton =  document.createElement("button")
+	randomButton.classList = "button clipboardtrigger has-icon"
+	randomButton.innerHTML = MAGIC_WORD
 
-    const ButtonSpace = document.createElement("li")
-    ButtonSpace.classList.add("panel-signin")
-    ButtonSpace.appendChild(randomButton);
-    ButtonSpace.onclick = start;
+	const ButtonSpace = document.createElement("li")
+	ButtonSpace.classList.add("panel-signin")
+	ButtonSpace.appendChild(randomButton);
+	ButtonSpace.onclick = start;
 
 
-    const PanelUl = document.getElementsByClassName("actions-panel")[0].getElementsByTagName("ul")[0];
-    const InsertTarget = PanelUl.getElementsByTagName("li")[0];
-    button =  PanelUl.insertBefore(ButtonSpace, InsertTarget);
+	const PanelUl = document.getElementsByClassName("actions-panel")[0].getElementsByTagName("ul")[0];
+	const InsertTarget = PanelUl.getElementsByTagName("li")[0];
+	button =  PanelUl.insertBefore(ButtonSpace, InsertTarget);
 }
 
 function getPagesCount(pageContent){
-    let pages = pageContent.getElementsByClassName("paginate-pages")[0]
-    if (!pages) { return 1 }
-    pages = pages.getElementsByTagName("li")
+	let pages = pageContent.getElementsByClassName("paginate-pages")[0]
+	if (!pages) { return 1 }
+	pages = pages.getElementsByTagName("li")
 
-    const maxPage = pages[pages.length - 1].outerText;
-    return maxPage;
+	const maxPage = pages[pages.length - 1].outerText;
+	return maxPage;
 }
 
 function getPageUrl(pageNumber){
-    let url = getListUrl(document.URL);
-    (pageNumber > 1) ?
-        url += "page/" + pageNumber:
-        url = url.split("page/")[0];
-    return url
+	let url = getListUrl(document.URL);
+	(pageNumber > 1) ?
+			url += "page/" + pageNumber:
+			url = url.split("page/")[0];
+	return url
 }
 
 function getPageNumber(URL){
@@ -183,25 +186,78 @@ function getPageNumber(URL){
 }
 
 function getListUrl(pageUrl){
-    let listUrl = pageUrl.toLowerCase();
+	let listUrl = pageUrl.toLowerCase();
 
-    if (listUrl.search("/?") != -1){
-        listUrl = pageUrl.split("?")[0];
-    }
+	if (listUrl.search("/?") != -1){
+			listUrl = pageUrl.split("?")[0];
+	}
 
-    if (listUrl.search("page/")){
-        listUrl = pageUrl.split("page/")[0];
-    }
+	if (listUrl.search("page/")){
+			listUrl = pageUrl.split("page/")[0];
+	}
 
-    return listUrl
+	return listUrl
 }
 
 function _showMoviePopup(){
-    const popup = document.createElement("ul");
-    popup.classList.add("smenu-menu");
-    popup.id = "random-popup";
-    popup.style = "position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); display: block;";
+	const popup = document.createElement("ul");
+	popup.classList.add("smenu-menu");
+	popup.id = "random-popup";
+	popup.style = "position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); display: block;";
 
-    document.getElementById("content").appendChild(popup);
-    return popup;
+	document.getElementById("content").appendChild(popup);
+	return popup;
+}
+
+function maximazeContent(){
+	const contentWraper = document.getElementById("content").getElementsByClassName(
+																									"content-wrap")[0];
+	const contentColWraper = contentWraper.getElementsByClassName("col-main")[0]
+	const gridElement = contentColWraper.getElementsByClassName("film-list")[0];
+
+	contentWraper.style = "width: auto; margin: 0em 2em";
+	contentColWraper.style = "width: 100%;";
+	gridElement.style = "height: 100vh; flex: auto; margin-right: 2em;";
+	gridElement.scrollIntoView();
+}
+
+function prepareGrid(keepIndex, keepCount){
+	const listGrid = document.getElementsByClassName("poster-list")[0];
+	const cards = listGrid.getElementsByClassName("poster-container");
+	const maxIndex = cards.length -1;
+	keepCount = Math.min(keepCount, cards.length);
+
+	let keep = [keepIndex]
+	for (i = 0; i < keepCount - 1; i++){
+
+			let randomNum = getRandomIntInclusive(0, maxIndex);
+			let searchIndex = 0;
+			while (keep.includes(randomNum)){
+					searchIndex++;
+
+					const forwardIndex = Math.min(randomNum + searchIndex, maxIndex);
+					if (!keep.includes(forwardIndex)){
+							randomNum = forwardIndex
+							break;
+					}
+
+					const backIndex = Math.max(randomNum - searchIndex, 0);
+					if (!keep.includes(backIndex)){
+							randomNum = backIndex
+							break;
+					}
+			}
+			keep.push(randomNum)
+	}
+
+	let toRemove = [];
+	for (i = 0; i <= maxIndex; i++){
+			if (!(keep.includes(i))){
+					cards[i].style = "display: none;";
+					toRemove.push(cards[i]);
+			}
+	}
+	for (i = 0; i < toRemove.length; i++){
+			toRemove[i].remove()
+	}
 }
