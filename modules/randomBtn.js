e0d72b7ca2a6b4da6d29fb101825a0b97b7d08ff
qtc <86272232+loop-nop@users.noreply.github.com>
2023-11-06 18:14:05 +0300

    some animation fixes

--- modules/randomBtn.js
+++ modules/randomBtn.js
@@ -3,15 +3,19 @@ This code will run ONLY on
     letterboxd/(username)/list/*
     letterboxd/(username)/watchlist/*
 */
+
+// code quality is getting much worse :)
+
 const MAGIC_WORD = "random"
 let button = null;
+const animationName = "spin";
+const fastRandom = true;
+
 let higlightedCard = null
 let zMemory = null
 let animId = null;
-const fastRandom = false;
 
 const style = `
-
 .lpGrid {
 	!important;
 	height: 100vh;
@@ -24,8 +28,13 @@ const style = `
 }
 
 .lpHiglight {
-	border: 5px pink solid;
-	z-index: 9001;
+	border: 0.5em gray solid;
+	box-shadow: 0px 0px 1em 0.5em gray;
+}
+
+.lpSelect {
+	!important;
+	box-shadow: 0px 0px 1em 0.5em lightblue;
 }
 
 .lpFloating {
@@ -34,21 +43,10 @@ const style = `
 	left: 50%;
 	top: 50%;
 }
-
-.lpCenter {
-	animation: test 5s 5s;
-	transform: translate(-50%, -50%);
-}
-
 `
 
 
 init()
-
-// test
-maximazeContent()
-const cards = prepareGrid(0, 10)
-CaruselCards(cards)
 
 function init(){
 	addStyle()
@@ -56,73 +54,91 @@ function init(){
 	const magicIndex = getMagicWord()
 	//console.debug(magicIndex)
 	if(magicIndex != null){
-			// let popup = showMoviePopup()
-			// popup.appendChild( getRandomMovie() )
-
-			openMovie(magicIndex)
-			return
+			selectMovie(magicIndex)
 	}else{
 			createRandomButton()
 	}
 }
 
 function start(){
-	button.disabled = true;
+	if (button.disabled){
+		return
+	}
+
 	const totalPages = getPagesCount(document);
-	if (totalPages > 1) {
-			if (fastRandom){
-					const randomPage = getRandomIntInclusive(1, totalPages);
-					goToMovie(randomPage)
-			}else{
-					const firstPageUrl = getPageUrl(1);
-					const paginationSize = getPageSize(firstPageUrl);
-					const lastPageUrl = getPageUrl(totalPages);
-					const lastPageSize = getPageSize(lastPageUrl);
-
-					Promise.all([paginationSize, lastPageSize]).then( (values) => {
-							const totalMoviesCount = values[0] * (totalPages - 1) + values[1];
-							const randomMovieIndex = getRandomIntInclusive(1, totalMoviesCount);
-							const moviePage = Math.floor(randomMovieIndex/values[0]);
-							const movieIndex = randomMovieIndex%values[0];
-
-							console.debug("pagination =", values[0], "lastPageSize =", values[1], "total:", totalMoviesCount, totalPages, randomMovieIndex, moviePage, movieIndex)
-							goToMovie(moviePage, movieIndex);
-					});
-			}
+	if (fastRandom){
+		const randomPage = getRandomIntInclusive(1, totalPages);
+		goToMovie(randomPage, -1)
 	}else{
-			goToMovie(1)
+		const firstPageUrl = getPageUrl(1);
+		const paginationSize = getPageSize(firstPageUrl);
+		const lastPageUrl = getPageUrl(totalPages);
+		const lastPageSize = getPageSize(lastPageUrl);
+		
+		button.disabled = true;
+
+		Promise.all([paginationSize, lastPageSize]).then( (values) => {
+			const totalMoviesCount = values[0] * (totalPages - 1) + values[1];
+			const randomMovieIndex = getRandomIntInclusive(1, totalMoviesCount);
+			const moviePage = Math.ceil(randomMovieIndex/values[0]);
+			const movieIndex = randomMovieIndex%values[0];
+
+			console.debug("pagination =", values[0], "lastPageSize =", values[1], "total:", totalMoviesCount,
+											totalPages, "randomIndex:",randomMovieIndex, moviePage, movieIndex);
+			button.disabled = false;
+			goToMovie(moviePage, movieIndex);
+		});
 	}
 }
 
-function goToMovie(pageNumber, movieIndex = -1){
-	const currentPage = getPageNumber(document.URL);
+function goToMovie(pageNumber, movieIndex){
+	const movies = getPageMovies(document);
+	if (movieIndex < 1 || movieIndex > movies.length){
+		movieIndex = getRandomIntInclusive(1, movies.length)
+		console.debug("new Index (goto)", movieIndex)
+	}
 	console.debug("goToMovie", pageNumber, movieIndex)
 
-	if (currentPage == pageNumber) {
-			openMovie(movieIndex);
+	const movieUrl = getPageUrl(pageNumber);
+	if (movieUrl){
+			const url  = [movieUrl, "?", MAGIC_WORD, "=", movieIndex].join("");
+			document.location.href = url;
 	}else{
-			const movieUrl = getPageUrl(pageNumber);
-			if (movieUrl){
-					const url  = [movieUrl, "?", MAGIC_WORD, "=", movieIndex].join("");
-					document.location.href = url;
-			}else{
-					document.search = "";
-					document.location.href = "https://letterboxd.com";
-			}
+			document.search = "";
+			document.location.href = "https://letterboxd.com";
 	}
 }
 
-function openMovie(index){
-	const movieCard = getMovieElement(index);
-	let url = movieCard.getElementsByClassName("film-poster")[0].getAttribute("data-target-link");
+function selectMovie(index){
+	const movies = getPageMovies(document);
+	if (index < 1 || index > movies.length){
+		index = getRandomIntInclusive(1, movies.length)
+		console.debug("new Index", index)
+	}
+
+	// for some reason film indexes starts at one.
+	const movie = movies[index - 1];
+
+	switch (animationName) {
+		case "spin":
+			maximazeContent()
+			animateCarusel(index - 1)
+			break;
+
+		default:
+			openMovie(movie)
+	}
+}
+
+function openMovie(movieElement){
+	let url = movieElement.getElementsByClassName("film-poster")[0].getAttribute("data-target-link");
 	if (url != null){
 			url = "https://letterboxd.com" + url;
 	}
 	else{
-			url = movieCard.getElementsByTagName("a")[0].href;
+			url = movieElement.getElementsByTagName("a")[0].href;
 	}
 
-	//console.debug(movieCard, "https://letterboxd.com" + url);
 	if (url){
 			document.search = "";
 			document.location.href = url;
@@ -157,23 +173,11 @@ async function getPageSize(url){
 }
 
 function getPageMovies(pageContent){
-	const movies = pageContent.getElementById("content").getElementsByClassName("poster-list")[0].getElementsByTagName("li")
-	return movies;
-}
+	const content = pageContent.getElementById("content")
+	const grid = getGrid(content)
+	const movies = grid.getElementsByTagName("li")
 
-function getMovieElement(index){
-	const movies = getPageMovies(document);
-	if (index < 1){
-			index = getRandomIntInclusive(1, movies.length)
-	}
-	
-	//console.debug(index,  movies[index-1]);
-	if (movies.length < index){
-			console.error("wrong movie index")
-			return
-	}
-	
-	return movies[index-1];
+	return movies;
 }
 
 function createRandomButton(){
@@ -252,9 +256,11 @@ function maximazeContent(){
 
 function prepareGrid(keepIndex, keepCount){
 	const listGrid = document.getElementsByClassName("poster-list")[0];
-	const cards = listGrid.getElementsByClassName("poster-container");
+	const cards = document.getElementsByClassName("poster-container");
 	const maxIndex = cards.length -1;
 	keepCount = Math.min(keepCount, cards.length);
+
+	//console.debug(listGrid, cards)
 
 	const keep = [keepIndex]
 	for (i = 0; i < keepCount - 1; i++){
@@ -297,53 +303,62 @@ function prepareGrid(keepIndex, keepCount){
 
 function highlightCard(card){
 	if (higlightedCard){
-		higlightedCard.classList.remove("lpHiglight");
+		higlightedCard.classList.remove("lpSelect");
 		higlightedCard.style.zIndex = zMemory;
 	}
 	zMemory = card.style.zIndex;
 	higlightedCard = card;
-	card.classList.add("lpHiglight");
+	card.classList.add("lpSelect");
 	card.style.zIndex = 9001;
 	//console.debug(card)
 }
 
+function getGrid(doc){
+	return doc.getElementsByClassName("film-list")[0]
+}
 
-function CaruselCards(cards){
-	const listGrid = document.getElementsByClassName("poster-list")[0];
-
+function animateCarusel(winnerIndex){
+	const cards = prepareGrid(winnerIndex, 10)
+	const listGrid = getGrid(document)
 	const space = (Math.PI * 2) / cards.length;
 	
 	for (i = 0; i < cards.length; i++){
 		const card = cards[i];
-		card.style.zIndex = getRandomIntInclusive(1, cards.length)
+		card.style.zIndex = getRandomIntInclusive(1, cards.length);
+		card.classList.add("lpHiglight");
+		//card.style.borderColor = "grey";
 	}
 
 	const rand = Math.random()
-	const fps = 60
+	const fps = 24
 	const frameTime = 1/fps
-	const animationTime = 2 + 2*rand
-	const initSpeed = 2
+	const animationTime = 5 + 2*rand
+	const initSpeed = 1.5
 	let time = -animationTime
 	let speed = initSpeed
+
+	const fontSize = Number(window.getComputedStyle(document.body).getPropertyValue('font-size').match(/\d+/)[0])
+	const margin = fontSize * 4
+	console.debug(fontSize, "margin",margin)
+	const cardSize = [cards[0].clientWidth, cards[0].clientHeight];
+
 	clearInterval(animId);
 	animId = setInterval(frame, frameTime*1000);
 	function frame() {
 		if (time > 0) {
 			clearInterval(animId);
 			highlightCard(cards[0]);
+			openMovie(cards[0]);
 		} else {
 			time += frameTime;
-			speed -= initSpeed * 1 / (animationTime*fps) * (rand+0.6)
-
-			const cardSize = [cards[0].clientWidth, cards[0].clientHeight];
+			speed -= initSpeed * 1 / (animationTime*fps) * (0.8 + rand/2)
 			const gridSize = [listGrid.clientWidth, listGrid.clientHeight];
 
-			const minDist = (Math.min(gridSize[0], gridSize[1]) - Math.max(cardSize[0], cardSize[1])) / 2
+			const minDist = (Math.min(gridSize[0] - margin, gridSize[1] - margin) - Math.max(cardSize[0], cardSize[1])) / 2
 
 			for (i = 0; i < cards.length; i++){
 				const card = cards[i];
 				card.classList.add("lpFloating");
-				//card.classList.add("lpCenter");
 
 				const d = time * speed + (space * i)
 				const x = minDist * Math.sin(d);
